7a92c7883bd7bd94e99ef814f660e329968834b1
SELFENGENHARIA\helder.fabio <heldr.macedo@gmail.com>
2018-05-28 10:13:43 -0300

    envio de email funcionando

---
+++ public/index.php
@@ -0,0 +1,40 @@
+<?php
+require_once '../vendor/autoload.php';
+use app\src\Mail;
+
+$email = new Mail;
+
+$email->send();
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--<form action="email.php" method="post">
+	<label for="Nome">Nome:</label>
+	<input type="text" name="Nome" size="35" />
+
+	<label for="Email">E-mail:</label>
+	<input type="text" name="Email" size="35" />
+
+	<label for="Fone">Telefone:</label>
+	<input type="text" name="Fone" size="35" />
+
+	<label for="Mensagem">Mensagem:</label>
+	<textarea name="Mensagem" rows="8" cols="40"></textarea>
+
+	<input type="submit" name="Enviar" value="Enviar" />
+</form>-->
